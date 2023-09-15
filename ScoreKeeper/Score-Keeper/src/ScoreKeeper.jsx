4e23b757c0de6ff20f4d6991b570fb1596b4ad76
asaiem <eng.abdulrahman.alsaiem@gmail.com>
2023-09-15 19:50:57 +0300

    ScoreKeeper Exercises

--- ScoreKeeper/Score-Keeper/src/ScoreKeeper.jsx
+++ ScoreKeeper/Score-Keeper/src/ScoreKeeper.jsx
@@ -2,7 +2,33 @@ import { useState } from "react";
 
 export default function ScoreKeeper({ num = 5, target = 6 }) {
     const [scores, setScores] = useState(new Array(num).fill(0))
+    
+    //Normal Way
+    function increaseScore(i) {
+        setScores(() => {
+            const copy = [...scores]
+            copy[i] += 1;
+            return copy
+        })
 
+    }
+
+    //Reacty Way
+    function increaseScore(i) {
+        setScores(() => {
+            scores.map((score,idx)=>{
+                if (i===idx){
+                    return score +=1;
+                }
+                //if not
+                return score    
+            })
+            const copy = [...scores]
+            copy[i] += 1;
+            return copy
+        })
+
+    }
 
 
     return (
@@ -13,7 +39,7 @@ export default function ScoreKeeper({ num = 5, target = 6 }) {
                     return (
                         <>
                             <li>Player{i + 1}: {score}</li>
-                            <button> +1 </button>
+                            <button  onClick={()=>increaseScore(i)}> +1 </button>
                         </>
 
                     )
